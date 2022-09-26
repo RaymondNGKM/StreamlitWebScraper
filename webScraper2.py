bf7e6209e5978410e9cecc588c39d502f8623269
Zahiruddin Zahidanishah <65801670+zzahir1978@users.noreply.github.com>
2022-09-26 18:42:55 +0800

    Add files via upload

---
+++ webScraper2.py
@@ -0,0 +1,38 @@
+import difflib
+import streamlit as st
+import pandas as pd
+import requests
+from bs4 import BeautifulSoup
+
+st.title('Python WebScraper')
+
+url = st.text_input('Please insert url:', key=1)
+st.write('Website:', url)
+with st.expander('Click to view:'):
+    if url:
+        res = requests.get(url)
+        content = BeautifulSoup(res.content, 'html.parser')
+        st.code(content)
+    else:
+        st.write('Loading...')
+
+htmlElement = st.text_input('Please insert filter element:', key=2)
+
+with st.expander('Click to view:'):
+    if htmlElement:
+            print = content.find_all({htmlElement})
+            st.code(print)
+    else:
+        st.write('Loading...')
+
+##quotes = content.find_all('div', class_='quote')
+##quote_file = []
+##for quote in quotes:
+    ##text = quote.find('span', class_='text').text
+    ##author = quote.find('small', class_='author').text
+    ##link = quote.find('a')
+    ##st.success(text)
+    ##st.write(author)
+    ##st.markdown(f"<a href=https://quotes.toscrape.com{link['href']}>{author}</a>", unsafe_allow_html=True)
+    ##st.code(f"https://quotes.toscrape.com{link['href']}")
+    ##quote_file.append([text,author,link['href']])
